74b11439f8a90830ca4eac565afa2d3573a7e23b
Dmytro Kharabara <dmitriy.kharabara@gmail.com>
2021-09-15 15:38:14 +0300

    Header created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,53 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Main page</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="dropdown0" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">File</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown08">
+                    <a class="dropdown-item" href="#">Import phone book</a>
+                    <a class="dropdown-item" href="#">Export phone book</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="dropdown1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Record</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown08">
+                    <a class="dropdown-item" href="#">Add</a>
+                    <a class="dropdown-item disabled" href="#">Edit</a>
+                    <a class="dropdown-item disabled" href="#">Delete</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Search</a>
+            </li>
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="#">Link</a>--}}
+{{--            </li>--}}
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link disabled" href="#">Disabled</a>--}}
+{{--            </li>--}}
+{{--            <li class="nav-item dropdown">--}}
+{{--                <a class="nav-link dropdown-toggle" id="dropdown08" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>--}}
+{{--                <div class="dropdown-menu" aria-labelledby="dropdown08">--}}
+{{--                    <a class="dropdown-item" href="#">Action</a>--}}
+{{--                    <a class="dropdown-item" href="#">Another action</a>--}}
+{{--                    <a class="dropdown-item" href="#">Something else here</a>--}}
+{{--                </div>--}}
+{{--            </li>--}}
+        </ul>
+    </div>
+</nav>
+
 <div class="table-responsive">
     <table class="table table-striped table-sm">
         <thead>
